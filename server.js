29838702960d4cf0137f8c5cce2a406cbc3e6849
lisagjh <lisagjhoorn@outlook.com>
2024-03-11 11:46:24 +0100

    start stories page

--- server.js
+++ server.js
@@ -32,61 +32,61 @@ app.use(express.urlencoded({ extended: true }));
 
 // Maak een GET route voor de home
 
-app.get('/', function (request, response) {
-
-  Promise.all([ // Fetch data from all endpoints concurrently using Promise.all()
-    fetchJson(apiUrl + '/tm_story'), // Fetch data from the tm_story endpoint
-    fetchJson(apiUrl + '/tm_language'), // Fetch data from the tm_language endpoint
-    fetchJson(apiUrl + '/tm_playlist'), // Fetch data from the tm_playlist endpoint
-    fetchJson(apiUrl + '/tm_audio') // Fetch data from the tm_audio endpoint
+app.get("/", function (request, response) {
+  Promise.all([
+    // Fetch data from all endpoints concurrently using Promise.all()
+    fetchJson(apiUrl + "/tm_story"), // Fetch data from the tm_story endpoint
+    fetchJson(apiUrl + "/tm_language"), // Fetch data from the tm_language endpoint
+    fetchJson(apiUrl + "/tm_playlist"), // Fetch data from the tm_playlist endpoint
+    fetchJson(apiUrl + "/tm_audio"), // Fetch data from the tm_audio endpoint
   ]).then(([storyData, languageData, playlistData, audioData]) => {
-// After all promises are resolved, this function will be executed with the fetched data
+    // After all promises are resolved, this function will be executed with the fetched data
 
-// Render the 'index.ejs' template and pass all fetched data to the view   
- response.render('home', {
+    // Render the 'index.ejs' template and pass all fetched data to the view
+    response.render("home", {
       stories: storyData.data, // Pass fetched story data to the view under the 'stories' key
       languages: languageData.data, // Pass fetched language data to the view under the 'languages' key
       playlists: playlistData.data, // Pass fetched playlist data to the view under the 'playlists' key
-      audio: audioData.data}) // Pass fetched audio data to the view under the 'audio' key
-    }); 
-  })
-
-// Maak een GET route voor de lessons pagina
-
-// app.get('/lessons', function (request, response) {
-
-//   Promise.all([
-//     fetchJson(apiUrl + '/tm_story'),
-//     fetchJson(apiUrl + '/tm_language'),
-//     fetchJson(apiUrl + '/tm_playlist'),
-//     fetchJson(apiUrl + '/tm_audio')
-//   ]).then(([storyData, languageData, playlistData, audioData]) => {
-    
-//     response.render('lessons', {
-//       stories: storyData.data,
-//       languages: languageData.data,
-//       playlists: playlistData.data,
-//       audio: audioData.data})
-//   })
-// })
+      audio: audioData.data,
+    }); // Pass fetched audio data to the view under the 'audio' key
+  });
+});
 
 // Maak een GET route voor de stories pagina
 
-app.get('/stories', function(request, response) {
+app.get("/stories", function (request, response) {
   Promise.all([
-    fetchJson(apiUrl + '/tm_story'),
-    fetchJson(apiUrl + '/tm_language'),
-    fetchJson(apiUrl + '/tm_playlist'),
-    fetchJson(apiUrl + '/tm_audio')
+    fetchJson(apiUrl + "/tm_story"),
+    fetchJson(apiUrl + "/tm_language"),
+    fetchJson(apiUrl + "/tm_playlist"),
+    fetchJson(apiUrl + "/tm_audio"),
   ]).then(([storyData, languageData, playlistData, audioData]) => {
-    
-    response.render('stories', {
+    response.render("stories", {
       stories: storyData.data,
       languages: languageData.data,
       playlists: playlistData.data,
-      audio: audioData.data})
-  })
-})
+      audio: audioData.data,
+    });
+  });
+});
+
+// GET route voor playlists
+
+app.get("/playlists", function (request, response) {
+  Promise.all([
+    fetchJson(apiUrl + "/tm_story"),
+    fetchJson(apiUrl + "/tm_language"),
+    fetchJson(apiUrl + "/tm_playlist"),
+    fetchJson(apiUrl + "/tm_audio"),
+  ]).then(([storyData, languageData, playlistData, audioData]) => {
+    response.render("playlists", {
+      stories: storyData.data,
+      languages: languageData.data,
+      playlists: playlistData.data,
+      audio: audioData.data,
+    });
+  });
+});
 
 // 3. Start de webserver
 // Stelt het port nummer in
